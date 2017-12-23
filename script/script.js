72b44a566a8a93619f85b15b68098047e4ec772c
olgaviktorovnaorlova <olgaviktorovnaorlova@gmail.om>
2017-12-23 04:14:31 +0200

    clean code

--- script/script.js
+++ script/script.js
@@ -1,56 +1,33 @@
-
-$(document).ready(function(){
-    $("#citroen").click(function(){
+$(document).ready(function () {
+    $("#citroen").click(function () {
         $("#mainDropdown").hide();
         $("#mainDropdown").show();
-
-
     });
-    $("#mazda").click(function(){
+    $("#mazda").click(function () {
         $("#mainDropdown").hide();
         $("#mainDropdown").show();
-
-
     });
-    $("#mercedes").click(function(){
+    $("#mercedes").click(function () {
         $("#mainDropdown").hide();
         $("#mainDropdown").show();
-
-
     });
 });
 
-// $(document).ready(function(){
-//     $("#citroen").click(function(){
-//         $(".currentCarIndecator").hide();
-//         $("#triangle-downMini").show();
-//     });
-//     $("#mazda").click(function(){
-//         $(".currentCarIndecator").hide();
-//         $("#triangle-downMidle").show();
-//     });
-//     $("#mercedes").click(function(){
-//         $(".currentCarIndecator").hide();
-//         $("#triangle-downPremium").show();
-//     });
-//
-// });
-
-$(document).ready(function(){
-    $("#citroen").click(function(){
+$(document).ready(function () {
+    $("#citroen").click(function () {
         $(".currentCarIndecator").hide();
 
         $("#triangle-downMini").show();
     });
 
-    $("#mazda").click(function(){
+    $("#mazda").click(function () {
         $(".currentCarIndecator").hide();
-         $("#triangle-downMidle").show();
-     });
-    $("#mercedes").click(function(){
+        $("#triangle-downMidle").show();
+    });
+    $("#mercedes").click(function () {
         $(".currentCarIndecator").hide();
-         $("#triangle-downPremium").show();
-     });
+        $("#triangle-downPremium").show();
+    });
 
     $('li.triangle_red').mouseenter(function () {
         $('li.triangle_red').removeClass("active");
